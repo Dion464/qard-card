c66329396bd86c5ef61592b996399d168a07168c
Dion Curri <curridion31@gmail.com>
2025-03-07 22:00:19 +0100

    test build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: true
+  },
+  // Add this if you're using experimental features
+  experimental: {
+    appDir: true
+  }
+};
 
 export default nextConfig;
