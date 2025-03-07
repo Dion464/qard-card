d33250fcebe994e5ac83315be0a1f062905dadd1
Dion Curri <curridion31@gmail.com>
2025-03-07 22:05:29 +0100

    trying to bulid

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,8 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  // Add this if you're using experimental features
-  experimental: {
-    appDir: true
-  }
+  reactStrictMode: true,
+  swcMinify: true
 };
 
 export default nextConfig;
