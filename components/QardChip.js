3b05ad5027f5f4e0f4d8c82c7cf02d8b7f462c20
Dion Curri <curridion31@gmail.com>
2025-03-11 16:37:27 +0100

    improved responsivity on mobile

--- components/QardChip.js
+++ components/QardChip.js
@@ -10,9 +10,11 @@ export default function QardChip() {
           Qard chip.
         </h1>
 
-        <div className="flex justify-between relative">
-          {/* Left Side Features */}
-          <div className="w-[30%] space-y-6 md:space-y-8">
+        {/* Modified layout structure for better mobile responsiveness */}
+        <div className="flex flex-col md:flex-row md:justify-between relative">
+          
+          {/* Left Side Features - Full width on mobile, 30% on larger screens */}
+          <div className="w-full md:w-[30%] space-y-6 md:space-y-8 mb-8 md:mb-0">
             <div className="flex gap-2 md:gap-4 items-start">
               <Image 
                 src="/protecionicon.svg"
@@ -68,9 +70,9 @@ export default function QardChip() {
             </div>
           </div>
 
-          {/* Center Chip Image */}
-          <div className="w-full flex justify-center">
-            <div className="relative w-full h-[400px] sm:h-[500px] md:h-[700px] lg:h-[900px] max-w-[1400px] mx-auto">
+          {/* Center Chip Image - Now positioned below text/icons on mobile */}
+          <div className="w-full md:order-none mb-8 md:mb-0">
+            <div className="relative w-full h-[250px] sm:h-[350px] md:h-[700px] lg:h-[900px] max-w-[1400px] mx-auto">
               <Image
                 src="/QardChip.svg"
                 alt="Qard Chip"
@@ -81,8 +83,8 @@ export default function QardChip() {
             </div>
           </div>
 
-          {/* Right Side Features */}
-          <div className="w-[30%] space-y-6 md:space-y-8">
+          {/* Right Side Features - Full width on mobile, 30% on larger screens */}
+          <div className="w-full md:w-[30%] space-y-6 md:space-y-8">
             <div className="flex gap-2 md:gap-4 items-start">
               <Image 
                 src="/protecionicon.svg"
